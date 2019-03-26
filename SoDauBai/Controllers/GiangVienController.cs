68ffc63c3732309ef6519df18f72de83fcd462d1
phamngocduy <phmngcduy@gmail.com>
2019-03-26 11:15:56 +0700

    Add list of all GiangVien

---
+++ SoDauBai/Controllers/GiangVienController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SoDauBai.Models;
+
+namespace SoDauBai.Controllers
+{
+    public class GiangVienController : Controller
+    {
+        private SoDauBaiEntities db = new SoDauBaiEntities();
+
+        public ActionResult Index()
+        {
+            return View(db.GiangViens.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(GiangVien giangVien)
+        {
+            if (ModelState.IsValid)
+            {
+                db.GiangViens.Add(giangVien);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(giangVien);
+        }
+
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GiangVien giangVien = db.GiangViens.Find(id);
+            if (giangVien == null)
+            {
+                return HttpNotFound();
+            }
+            return View(giangVien);
+        }
+
+        // POST: GiangVien/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Email,MaGV,HocVi,HoTen")] GiangVien giangVien)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(giangVien).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(giangVien);
+        }
+
+        // GET: GiangVien/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            GiangVien giangVien = db.GiangViens.Find(id);
+            if (giangVien == null)
+            {
+                return HttpNotFound();
+            }
+            return View(giangVien);
+        }
+
+        // POST: GiangVien/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            GiangVien giangVien = db.GiangViens.Find(id);
+            db.GiangViens.Remove(giangVien);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
