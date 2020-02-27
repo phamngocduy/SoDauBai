0abd0de01ad5741b64ea4bec50dee0fb5c150193
phamngocduy <phmngcduy@gmail.com>
2020-02-27 16:57:36 +0700

    Allow to see list ThoiKhoaBieu of PhuGiang

--- SoDauBai/Controllers/HomeController.cs
+++ SoDauBai/Controllers/HomeController.cs
@@ -22,10 +22,12 @@ namespace SoDauBai.Controllers
         {
             var hk = this.GetHocKy(db);
             var email = User.Identity.GetUserName();
-            var MaMH_NhomTo = (from tkb in db.ThoiKhoaBieux
-                            join gv in db.GiangViens on tkb.MaGV equals gv.MaGV
+            var MaMH_NhomTo = (from tkb in db.ThoiKhoaBieux join gv in db.GiangViens on tkb.MaGV equals gv.MaGV
                             where tkb.HocKy == hk && (gv.Email == email || tkb.BanCanSus.Count(bcs => bcs.Email == email) > 0)
-                            select tkb.MaMH + "_" + tkb.NhomTo).ToList();
+                            select tkb) // next for PhuGiang
+                            .Union(from pg in db.PhuGiangs join gv in db.GiangViens on pg.MaGV equals gv.MaGV
+                                where gv.Email == email && pg.ThoiKhoaBieu.HocKy == hk select pg.ThoiKhoaBieu)
+                            .Select(tkb => tkb.MaMH + "_" + tkb.NhomTo).ToList();
             var model = db.ThoiKhoaBieux.Where(tkb => MaMH_NhomTo.Contains(tkb.MaMH + "_" + tkb.NhomTo) && tkb.HocKy == hk);
             ViewBag.GVs = db.GiangViens.ToList();
             return View(model.ToList());
