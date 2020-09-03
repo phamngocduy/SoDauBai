fabcc379d9bd5aa21136bc8afe82c21b2d1c3bd6
phamngocduy <phmngcduy@gmail.com>
2020-09-03 13:23:57 +0700

    Fix all emails in system to new format

--- SoDauBai/Controllers/HomeController.cs
+++ SoDauBai/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Web;
 using System.Linq;
 using System.Web.Mvc;
 using SoDauBai.Models;
@@ -33,18 +34,74 @@ namespace SoDauBai.Controllers
                         user.UserName = user.Email = loginInfo.Email;
                         db.Entry(user).State = EntityState.Modified;
                     }
+
                     var giangVien = db.GiangViens.SingleOrDefault(gv => gv.Email == email);
                     if (giangVien != null)
                     {
                         giangVien.Email = loginInfo.Email;
                         db.Entry(giangVien).State = EntityState.Modified;
                     }
+                    var giaoVu = db.GiaoVus.SingleOrDefault(gv => gv.Email == email);
+                    if (giaoVu != null)
+                    {
+                        giaoVu.Email = loginInfo.Email;
+                        db.Entry(giaoVu).State = EntityState.Modified;
+                    }
+
                     var banCanSu = db.BanCanSus.Where(bcs => bcs.Email == email);
                     banCanSu.ForEach(bcs => bcs.Email = loginInfo.Email);
                     banCanSu.ForEach(bcs => db.Entry(bcs).State = EntityState.Modified);
+
+                    var guiEmail = db.GuiEmails.Where(gm => gm.Email == email || gm.FromTo == email);
+                    guiEmail.ForEach(gm =>
+                    {
+                        if (gm.Email == email) gm.Email = loginInfo.Email;
+                        if (gm.FromTo == email) gm.FromTo = loginInfo.Email;
+                        db.Entry(gm).State = EntityState.Modified;
+                    });
+
+                    var lienHe = db.LienHes.Where(lh => lh.from == email || lh.to == email);
+                    lienHe.ForEach(lh =>
+                    {
+                        if (lh.from == email) lh.from = loginInfo.Email;
+                        if (lh.to == email) lh.to = loginInfo.Email;
+                        db.Entry(lh).State = EntityState.Modified;
+                    });
+
+                    var phongDayBu = db.PhongDayBus.Where(pdb => pdb.email1 == email || pdb.email2 == email);
+                    phongDayBu.ForEach(pdb =>
+                    {
+                        if (pdb.email1 == email) pdb.email1 = loginInfo.Email;
+                        if (pdb.email2 == email) pdb.email2 = loginInfo.Email;
+                        db.Entry(pdb).State = EntityState.Modified;
+                    });
+
+                    var soGhiBai = db.SoGhiBais.Where(sgb => sgb.Email == email);
+                    soGhiBai.ForEach(sgb =>
+                    {
+                        sgb.Email = loginInfo.Email;
+                        db.Entry(sgb).State = EntityState.Modified;
+                    });
+
+                    var soGhiChu = db.SoGhiChus.Where(sgc => sgc.Email == email);
+                    soGhiChu.ForEach(sgc =>
+                    {
+                        sgc.Email = loginInfo.Email;
+                        db.Entry(sgc).State = EntityState.Modified;
+                    });
+
+                    var troGiang = db.TroGiangs.Where(tg => tg.Email.Contains(email));
+                    troGiang.ForEach(tg =>
+                    {
+                        tg.Email = tg.Email.Replace(email, loginInfo.Email);
+                        db.Entry(tg).State = EntityState.Modified;
+                    });
+
                     db.SaveChanges();
                     email = loginInfo.Email;
                     ViewBag.Message = "Successfully transfer to email: " + email;
+                    HttpContext.GetOwinContext().Get<ApplicationSignInManager>()
+                        .ExternalSignIn(loginInfo, isPersistent: false);
                 }
             }
 
